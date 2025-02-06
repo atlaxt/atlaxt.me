f9ee54d6bca7582eae632b1d62737fa9f0f44b33
eiskaltDEV <atlasyigitaydin@gmail.com>
2025-02-06 14:49:28 +0300

    fix speedSpeedInsights

--- app.vue
+++ app.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts">
 import { NuxtLayout, NuxtPage } from '#components'
 import { Analytics } from '@vercel/analytics/nuxt'
-import { SpeedInsights } from '@vercel/speed-insights/nuxt'
 
 useHead({
   title: 'Atlas Yiğit Aydın',
@@ -13,5 +12,4 @@ useHead({
     <NuxtPage />
   </NuxtLayout>
   <Analytics />
-  <SpeedInsights />
 </template>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,13 @@
+<script setup lang="ts">
+import { SpeedInsights } from '@vercel/speed-insights/nuxt'
+</script>
+
 <template>
   <div class="w-screen justify-center flex h-screen fixed left-0 top-0 bg-black text-white doto">
     <div class="flex flex-col items-start justify-around pt-12 px-6 relative lg:w-96 md:w-10/12 w-full">
       <slot />
     </div>
+    <SpeedInsights />
   </div>
 </template>
 
