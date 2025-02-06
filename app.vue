bb4fe99c2561f16103ad519d144a809962c9262b
eiskaltDEV <atlasyigitaydin@gmail.com>
2025-02-06 12:45:02 +0300

    projeler tanıtıldı

--- app.vue
+++ app.vue
@@ -1,9 +1,65 @@
 <script setup lang="ts">
+import Link from '@/components/link.vue'
 
+const colorMode = useColorMode()
+
+onMounted(() => {
+  colorMode.preference = 'light'
+})
 </script>
 
 <template>
-  <div class="fixed top-0 left-0 w-screen h-screen flex items-center justify-center font-extralight text-2xl bg-zinc-900 text-white">
-    me
+  <div class="w-screen justify-center text-lg flex h-screen fixed left-0 top-0 bg-zinc-900 text-white doto">
+    <div class="flex flex-col justify-around pt-12 px-6">
+      <label> ATLAS YİĞİT AYDIN </label>
+
+      <div class="flex flex-col gap-4">
+        <Link
+          v-for="(link, key) in [
+            {
+              label: 'STARS',
+              links: {
+                live: 'https://stars.atlasyigitaydin.com/',
+                github: 'https://github.com/atlasyigitaydin/stars',
+              },
+            },
+            {
+              label: 'QR',
+              links: {
+                live: 'https://stars.atlasyigitaydin.com/',
+                github: 'https://github.com/atlasyigitaydin/qr',
+              },
+            },
+            {
+              label: 'FOXIAN',
+              links: {
+                live: 'https://stars.atlasyigitaydin.com/',
+                github: 'https://github.com/atlasyigitaydin/foxian',
+              },
+            },
+          ]"
+          :key
+          :label="link.label"
+          :links="link.links"
+        />
+      </div>
+
+      <div class="opacity-70">
+        <label> this page is under construction </label>
+      </div>
+    </div>
   </div>
 </template>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Doto&display=swap');
+
+.doto {
+  font-family: "Doto", serif;
+  font-optical-sizing: auto;
+  font-weight: 400;
+  font-style: normal;
+  font-variation-settings:
+    "ROND" 0;
+}
+</style>
---
+++ components/link.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+const props = defineProps<{
+  label: string
+  links: {
+    live: string
+    github: string
+  }
+}>()
+const isHover = ref<boolean>(false)
+</script>
+
+<template>
+  <div
+    class="relative flex items-center justify-between"
+    @mouseenter="isHover = true"
+    @mouseleave="isHover = false"
+  >
+    <div
+      :class="{
+        'ml-2': isHover,
+      }"
+      class="w-full animation-all duration-500 ease-in-out delay-100"
+    >
+      {{ props.label }}
+    </div>
+
+    <div class="w-full flex justify-end gap-4">
+      <a
+        :href="props.links.github"
+        target="_blank"
+        class="flex items-center gap-2"
+      >
+        <Icon class="text-lg opacity-70" name="pixelarticons:github-2" />
+      </a>
+
+      <a
+        :href="props.links.live"
+        target="_blank"
+        class="flex items-center gap-2"
+      >
+        <Icon class="text-lg opacity-70" name="pixelarticons:external-link" />
+      </a>
+    </div>
+  </div>
+</template>
