109ec3456b7f1ff90d20ee8aef843fa6d38f3518
eiskaltDEV <atlasyigitaydin@gmail.com>
2025-02-06 14:12:01 +0300

    github link size change

--- components/my-github-link.vue
+++ components/my-github-link.vue
@@ -2,7 +2,7 @@
   <a
     href="https://github.com/atlasyigitaydin"
     target="_blank"
-    class="flex items-center gap-2 text-sm"
+    class="flex items-center gap-2 text-xs"
   >
     <p> atlasyigitaydin </p>
     <Icon class="text-lg opacity-70" name="pixelarticons:github-2" />
