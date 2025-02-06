bed2eadc199d4a28e8d824cee134edd3b66f9da3
eiskaltDEV <atlasyigitaydin@gmail.com>
2025-02-06 14:17:11 +0300

    cancel glitch

--- app.vue
+++ app.vue
@@ -9,8 +9,8 @@ useHead({
 
 <template>
   <div class="w-screen justify-center flex h-screen fixed left-0 top-0 bg-black text-white doto">
-    <div class="flex flex-col lg:items-center items-start justify-around pt-12 px-6 relative">
-      <label data-text="ATLAS YİĞİT AYDIN" class="glitch"> ATLAS YİĞİT AYDIN </label>
+    <div class="flex flex-col items-start justify-around pt-12 px-6 relative lg:w-96 w-full">
+      <label> ATLAS YİĞİT AYDIN </label>
 
       <div class="flex flex-col gap-4 w-full">
         <Link
@@ -45,7 +45,7 @@ useHead({
         />
       </div>
 
-      <div class="opacity-70">
+      <div class="opacity-70 text-yellow-300">
         <label> this page is under construction </label>
       </div>
 
@@ -64,60 +64,5 @@ useHead({
   font-style: normal;
   font-variation-settings:
     "ROND" 0;
-}
-</style>
-
-<style scoped>
-.glitch {
-  position: relative;
-  font-size: 12p6;
-  font-weight: bold;
-  color: #ffffff;
-  letter-spacing: 3px;
-  z-index: 1;
-}
-
-.glitch:before,
-.glitch:after {
-  display: block;
-  content: attr(data-text);
-  position: absolute;
-  top: 0;
-  left: 0;
-  opacity: 0.8;
-}
-
-.glitch:before {
-  animation: glitch-it 0.3s cubic-bezier(0.25, 0.46, 0.45, 0.94) both infinite;
-  color: #00ffff;
-  z-index: -1;
-}
-
-.glitch:after {
-  animation: glitch-it 0.3s cubic-bezier(0.25, 0.46, 0.45, 0.94) reverse both
-    infinite;
-  color: #ff00ff;
-  z-index: -2;
-}
-
-@keyframes glitch-it {
-  0% {
-    transform: translate(0);
-  }
-  20% {
-    transform: translate(-2px, 2px);
-  }
-  40% {
-    transform: translate(-2px, -2px);
-  }
-  60% {
-    transform: translate(2px, 2px);
-  }
-  80% {
-    transform: translate(2px, -2px);
-  }
-  to {
-    transform: translate(0);
-  }
 }
 </style>
