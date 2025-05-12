09e94243ec73458bdfebf471bd5eca45f99c2f2e
eiskaltDEV <atlasyigitaydin@gmail.com>
2025-05-12 13:50:03 +0300

    'deploy'

--- components/app/header.vue
+++ components/app/header.vue
@@ -21,7 +21,7 @@ onMounted(() => {
       >
     </RouterLink>
 
-    <nav class="flex md:flex-row flex-col-reverse md:items-center items-end md:gap-8 gap-4" aria-label="Main navigation">
+    <nav class="flex flex-row items-center md:gap-8 gap-4" aria-label="Main navigation">
       <div class="flex flex-row md:gap-8 gap-4 items-center">
         <RouterLink
           class="opacity-70 hover:opacity-100 flex flex-row items-center gap-2 duration-300"
@@ -58,19 +58,18 @@ onMounted(() => {
         >
           <Icon class="text-lg" :name="social.icon" aria-hidden="true" />
         </a>
-
-        <button
-          class="cursor-pointer opacity-70 hover:opacity-100 duration-300 flex items-center"
-          :aria-label="isDark ? 'Switch to light theme' : 'Switch to dark theme'"
-          @click="toggleTheme"
-        >
-          <Icon
-            class="text-xl"
-            :name="`${isDark ? 'lucide:moon' : 'lucide:sun'}`"
-            aria-hidden="true"
-          />
-        </button>
       </div>
+      <button
+        class="cursor-pointer opacity-70 hover:opacity-100 duration-300 flex items-center"
+        :aria-label="isDark ? 'Switch to light theme' : 'Switch to dark theme'"
+        @click="toggleTheme"
+      >
+        <Icon
+          class="text-xl"
+          :name="`${isDark ? 'lucide:moon' : 'lucide:sun'}`"
+          aria-hidden="true"
+        />
+      </button>
     </nav>
   </header>
   <div v-else class="h-24" aria-hidden="true" />
