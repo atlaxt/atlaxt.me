af352ea5cf39a4aef377cc9576c2cadbcbc2b7e4
Atlas <yigitaydin@trex.com.tr>
2025-08-19 14:22:13 +0300

    'deploy'

--- app/pages/projects.vue
+++ app/pages/projects.vue
@@ -16,12 +16,7 @@ const sections = computed(() => {
 
 <template>
   <div class="lg:w-3xl lg:px-0 px-2 flex flex-col gap-6 mx-auto mt-12 content-wrapper">
-      <section
-        v-for="([key, items]) in sections"
-        :id="key"
-        :key="key"
-        class="flex flex-col gap-6"
-      >
+    <section v-for="([key, items]) in sections" :id="key" :key="key" class="flex flex-col gap-6">
       <a :href="`#${key}`" class="text-2xl group relative flex flex-row gap-2 items-center">
         <h2 class="font-semibold tracking-tight">
           {{ formatTitle(key) }}
@@ -29,11 +24,7 @@ const sections = computed(() => {
         <span class="opacity-0 ml-1 group-hover:opacity-80 transition-opacity">#</span>
       </a>
       <div class="flex flex-col gap-4">
-        <CardProject
-          v-for="item in items"
-          :key="item.title"
-          :project="item"
-        />
+        <CardProject v-for="item in items" :key="item.title" :project="item" />
       </div>
     </section>
   </div>
